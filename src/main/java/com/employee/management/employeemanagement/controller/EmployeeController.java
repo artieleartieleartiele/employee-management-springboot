d721f9413382a35287d9688796070d244a768417
Lester Jan <lesterjanartienda@gmail.com>
2021-02-15 01:41:39 +0800

    add employee

--- src/main/java/com/employee/management/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/employee/management/employeemanagement/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.employee.management.employeemanagement.controller;
 import com.employee.management.employeemanagement.model.Employee;
 import com.employee.management.employeemanagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class EmployeeController {
     @GetMapping("employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("employees")
+    public Employee addEmployee(@RequestBody Employee payload) {
+        return employeeService.addEmployee(payload);
     }
 }
--- src/main/java/com/employee/management/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/employee/management/employeemanagement/service/EmployeeService.java
@@ -15,4 +15,8 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
